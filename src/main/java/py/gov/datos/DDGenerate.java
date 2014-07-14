d3a76ebdd380ec4bad8df23be19f91c353fa72c2
Rodrigo Parra <rodpar07@gmail.com>
2014-07-14 14:25:54 -0400

    Initial commit

---
+++ src/main/java/py/gov/datos/DDGenerate.java
@@ -0,0 +1,98 @@
+package py.gov.datos;
+
+/*
+ * @author	Rodrigo Parra	
+ * @copyright	2014 Governance and Democracy Program USAID-CEAMSO
+ * @license 	http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * USAID-CEAMSO
+ * Copyright (C) 2014 Governance and Democracy Program
+ * http://ceamso.org.py/es/proyectos/20-programa-de-democracia-y-gobernabilidad
+ * 
+----------------------------------------------------------------------------
+ * This file is part of the Governance and Democracy Program USAID-CEAMSO,
+ * is distributed as free software in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. You can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License version 2 as published by the 
+ * Free Software Foundation, accessible from <http://www.gnu.org/licenses/> or write 
+ * to Free Software Foundation (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, 
+ * MA 02111-1301, USA.
+ ---------------------------------------------------------------------------
+ * Este archivo es parte del Programa de Democracia y Gobernabilidad USAID-CEAMSO,
+ * es distribuido como software libre con la esperanza que sea de utilidad,
+ * pero sin NINGUNA GARANTÍA; sin garantía alguna implícita de ADECUACION a cualquier
+ * MERCADO o APLICACION EN PARTICULAR. Usted puede redistribuirlo y/o modificarlo 
+ * bajo los términos de la GNU Lesser General Public Licence versión 2 de la Free 
+ * Software Foundation, accesible en <http://www.gnu.org/licenses/> o escriba a la 
+ * Free Software Foundation (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, 
+ * MA 02111-1301, USA.
+ */
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.*;
+
+public class DDGenerate {
+    private final Logger LOG = LoggerFactory.getLogger(DDGenerate.class);
+
+    @Parameter(description = "Files")
+    private List<String> files = new ArrayList<String>();
+
+    @Parameter(names = {"-o", "--output"}, description = "Destination path")
+    private String out = "";
+
+    @Parameter(names = {"-f", "--format"}, description = "Desired output format")
+    private String format = "html";
+
+    private FileConverterFactory f = new FileConverterFactory();
+
+
+    private static final Map<String, List<FileConverterType>> chainMap;
+
+    static {
+        chainMap = new HashMap<String, List<FileConverterType>>();
+        chainMap.put("html", Arrays.asList(new FileConverterType[]
+                {FileConverterType.XLS_TO_CSV, FileConverterType.CSV_TO_HTML}));
+        chainMap.put("csv", Arrays.asList(new FileConverterType[]
+                {FileConverterType.XLS_TO_CSV}));
+    }
+
+    public void generate() {
+        List<FileConverterType> chain = chainMap.get(format);
+        for (String filename : files) {
+            File file = new File(filename);
+            List<File> convertee = new ArrayList<File>();
+            convertee.add(file);
+            generate(convertee, chain);
+        }
+        LOG.info("Success!");
+    }
+
+    private void generate(List<File> files, List<FileConverterType> chain) {
+        if (!chain.isEmpty()) {
+            for (File file : files) {
+                List<File> tmp = generate(file, chain.get(0));
+
+                generate(tmp, chain.subList(1, chain.size()));
+            }
+        }
+    }
+
+    private List<File> generate(File file, FileConverterType type) {
+        FileConverter converter = f.getFileConverter(type);
+        List<File> results = converter.convert(file, out);
+        return results;
+    }
+
+
+    public static void main(String[] args) {
+        DDGenerate generator = new DDGenerate();
+        new JCommander(generator, args);
+        generator.generate();
+    }
+}
---
+++ src/main/java/py/gov/datos/XlsToCsvConverter.java
@@ -0,0 +1,128 @@
+package py.gov.datos;
+
+/*
+ * @author	Rodrigo Parra	
+ * @copyright	2014 Governance and Democracy Program USAID-CEAMSO
+ * @license 	http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * USAID-CEAMSO
+ * Copyright (C) 2014 Governance and Democracy Program
+ * http://ceamso.org.py/es/proyectos/20-programa-de-democracia-y-gobernabilidad
+ * 
+----------------------------------------------------------------------------
+ * This file is part of the Governance and Democracy Program USAID-CEAMSO,
+ * is distributed as free software in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. You can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License version 2 as published by the 
+ * Free Software Foundation, accessible from <http://www.gnu.org/licenses/> or write 
+ * to Free Software Foundation (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, 
+ * MA 02111-1301, USA.
+ ---------------------------------------------------------------------------
+ * Este archivo es parte del Programa de Democracia y Gobernabilidad USAID-CEAMSO,
+ * es distribuido como software libre con la esperanza que sea de utilidad,
+ * pero sin NINGUNA GARANTÍA; sin garantía alguna implícita de ADECUACION a cualquier
+ * MERCADO o APLICACION EN PARTICULAR. Usted puede redistribuirlo y/o modificarlo 
+ * bajo los términos de la GNU Lesser General Public Licence versión 2 de la Free 
+ * Software Foundation, accesible en <http://www.gnu.org/licenses/> o escriba a la 
+ * Free Software Foundation (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, 
+ * MA 02111-1301, USA.
+ */
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class XlsToCsvConverter implements FileConverter{
+
+    private final  Logger LOG = LoggerFactory.getLogger(XlsToCsvConverter.class);
+
+    @Override
+    public List<File> convert(File file, String path) {
+        List<File> result = new ArrayList<File>();
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(file));
+            for(int i= 0; i < workbook.getNumberOfSheets(); i++){
+                XSSFSheet sheet = workbook.getSheetAt(i);
+                File outputFile = new File(path + sheet.getSheetName() + ".csv");
+                if(outputFile.createNewFile()){
+                    FileOutputStream out = new FileOutputStream(outputFile);
+                    StringBuffer content = this.convertSheet(sheet);
+                    out.write(content.toString().getBytes());
+                    result.add(outputFile);
+                }else{
+                    LOG.error("Can not create output file");
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private StringBuffer convertSheet(XSSFSheet sheet){
+        StringBuffer data = new StringBuffer();
+        Iterator<Row> rowIterator = sheet.iterator();
+
+        while(rowIterator.hasNext()){
+            Row row = rowIterator.next();
+            StringBuffer rowBuffer = convertRow(row);
+            data.append(rowBuffer);
+        }
+
+        return data;
+    }
+
+    private StringBuffer convertRow(Row row){
+        StringBuffer data = new StringBuffer();
+        Iterator<Cell> cellIterator = row.cellIterator();
+
+        while (cellIterator.hasNext()){
+            StringBuffer cellBuffer = convertCell(cellIterator.next());
+            data.append(cellBuffer);
+        }
+        if(data.length() > 0){
+            data = data.deleteCharAt(data.length() - 1);
+        }
+        data.append("\r\n");
+        return data;
+    }
+
+    private StringBuffer convertCell(Cell cell){
+        StringBuffer data = new StringBuffer();
+
+        switch (cell.getCellType()) {
+            case Cell.CELL_TYPE_BOOLEAN:
+                data.append(cell.getBooleanCellValue() + ";");
+
+                break;
+            case Cell.CELL_TYPE_NUMERIC:
+                data.append(cell.getNumericCellValue() + ";");
+
+                break;
+            case Cell.CELL_TYPE_STRING:
+                data.append("\"" + cell.getStringCellValue() + "\";");
+                break;
+
+            case Cell.CELL_TYPE_BLANK:
+                data.append("" + ";");
+                break;
+            default:
+                data.append(cell + ";");
+        }
+
+        return data;
+    }
+}
